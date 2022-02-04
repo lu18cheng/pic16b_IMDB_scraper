202866f5cce2985ee2d1a78ee9c173ece9e192f8
lu18cheng <59773993+lu18cheng@users.noreply.github.com>
2022-02-03 17:02:51 -0800

    Update imdb_spider.py

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -11,13 +11,13 @@ class ImdbSpider(scrapy.Spider):
 
     def parse(self, response):
         '''
-        Assume that you start on a movie page, and then navigate to the Cast & Crew page
-        :param response:
-        :return: None
+        Assume that you start on a movie page
+        then navigate to the Cast & Crew page
         '''
 
         elts = response.css("li.ipc-inline-list__item")
-        links = [e.css("a").attrib['href'] for e in elts if e.css("a::text") and e.css("a::text").get() == "Cast & crew"]
+        links = [e.css("a").attrib['href'] for e in elts
+                 if e.css("a::text") and e.css("a::text").get() == "Cast & crew"]
         yield scrapy.Request(response.urljoin(links[0]), callback=self.parse_full_credits)
         # yield scrapy.Request(response.urljoin("fullcredits/"), callback=self.parse_full_credits)
 
@@ -25,24 +25,34 @@ class ImdbSpider(scrapy.Spider):
     def parse_full_credits(self, response):
         '''
         assume that you start on the Cast & Crew page.
-        Its purpose is to yield a `scrapy.Request` for the page of each actor listed on the page
+        yield a `scrapy.Request` for the page of each actor listed on the page
         '''
 
+        #get link to all cast members
         cast_links = [a.attrib['href'] for a in response.css("table.cast_list td.primary_photo a")]
+
+        # yield result
         for link in cast_links:
-            yield scrapy.Request(response.urljoin(link), callback=self.parse_actor_page)
+            yield scrapy.Request(response.urljoin(link),
+                                 callback=self.parse_actor_page)
 
     def parse_actor_page(self, response):
         '''
-        assume that you start on the page of an actor. I
-        yield a dictionary with two key-value pairs, of the form {"actor" : actor_name, "movie_or_TV_name" : movie_or_TV_name}.
+        assume that you start on the page of an actor.
+        yield a dictionary with two key-value pairs of the form
+        {"actor" : actor_name,
+        "movie_or_TV_name" : movie_or_TV_name}.
         '''
 
+        #get name of actor
         actor = response.css("h1.header span.itemprop::text").get()
 
+        #get fields storing information of all films
         rows = response.css("div.filmo-row")
+        # filter by `attr(id)` to get only main films and eliminate episodes information
         films = [e.css("a::text").get() for e in rows if e.css("::attr(id)") and e.css("a")]
 
+        # yield result
         for film in films:
             yield {"actor": actor, "movie_or_TV_name": film}
 
